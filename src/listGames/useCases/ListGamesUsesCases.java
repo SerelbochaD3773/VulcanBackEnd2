3207b2b3d512db2496c364a7fb31ba6eccc54aec
01paulina <paulinalondonodiaz01@gmail.com>
2025-09-19 19:17:03 -0500

    Remove unused binary files and implement update and delete methods in ListGamesUsesCases

--- src/listGames/useCases/ListGamesUsesCases.java
+++ src/listGames/useCases/ListGamesUsesCases.java
@@ -27,6 +27,37 @@ public class ListGamesUsesCases {
         catch (Exception e) {
             return "Ha ocurrido un error, por favor intentelo nuevo";
         }
+
+        // UPDATE
+    public String update(String idGame, ListGames ListGameIn) { //tarea entrante
+        try {
+            Task listGameFound = new ListGames();
+            for (int i = 0; i < ListGames.size(); i++) {
+               if(index == i){
+                ListGames listGames = listGames.get(index); // tarea existente
+                listGames.setNameGame(ListGameIn.getNameGame());
+                listGames.setDescriptionGame(ListGameInn.getDescriptionGame());
+                listGames.setStatusGame(ListGameIn.getStatusGame());
+                listGamesFound = listGames;
+               }
+            }
+            return "Juego Actualizado:" + listGames.getNameGame() + " Descripcion del Juego:" + listGameFound.getDescriptionGame();
+        }
+        catch (Exception e) {
+            return "Ha ocurrido un error, por favor intentelo nuevo";
+        }
+    }
+
+    // Delete
+    public String delete(String idGame){
+       try {
+            tasks.remove(index);
+            return "Se ha eliminado el juego correctamente.";
+       } catch (Exception e) {
+            return "no ha sido posible eliminar el juego.";
+       }
+    
+    }
     }
   
 }
