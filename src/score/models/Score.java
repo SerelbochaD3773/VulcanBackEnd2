3e8417f94d50a5d2d8f71545d50040c98ef28abd
MarioMunera <samuelmariomunera@gmail.com>
2025-09-26 21:48:32 -0500

    Add models and use cases for PreferenciaMateria and Score management

---
+++ src/score/models/Score.java
@@ -0,0 +1,11 @@
+package score.models;
+
+public class Score {
+    private int point;
+    private int user;
+
+    public Score(int point, int user) {
+        this.point = point;
+        this.user = user;
+    }
+}
