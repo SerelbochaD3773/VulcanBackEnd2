601faadf52d0ebcddf5389565e3a02eac95a5746
MarioMunera <samuelmariomunera@gmail.com>
2025-10-19 22:28:43 -0500

    Add Feedback feature with controller, model, and use case implementations

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
-import listGames.controllers.ListGamesController; 
+import feedback.controllers.FeedbackController;//Mario
+import listGames.controllers.ListGamesController; //albani
 import score.controllers.ScoreController; //ROQUE
 //importe su controlador aqui
 
@@ -7,9 +8,10 @@ public class App {
   public static void main(String[] args) throws Exception {
     ListGamesController listGamesController = new ListGamesController(); 
     ScoreController scoreController = new ScoreController(); // Instanciar el controlador de puntajes = ROQUE
+    FeedbackController feedbackController = new FeedbackController();
     //Aqui instanciar su propio objeto controller
 
-    // Instanciamos la listGameController
+    //######################################################### ALBANY #########################################################
     // CREATE - Crear un Juego
     System.out.println("Se procede a crear un juego:");
     String gameOne = listGamesController.create("1", "Juego Ranita", "Codigos Flex- Css", true);
@@ -32,51 +34,51 @@ public class App {
     System.out.println(deleteListGame);
     System.out.println("\n");
 
-    //ROQUE
+    //######################################################### ROQUE #########################################################
     System.out.println("\n GESTION DE PUNTAJES\n");
 
-        // CREATE - Crear puntajes para estudiantes
-        System.out.println("Se procede a crear puntajes para estudiantes...");
-        String scoreOne = scoreController.create(1010, 85);
-        System.out.println(scoreOne);
-        
-   
+    // CREATE - Crear puntajes para estudiantes
+    System.out.println("Se procede a crear puntajes para estudiantes...");
+    String scoreOne = scoreController.create(1010, 85);
+    System.out.println(scoreOne);
+    
+    // READ ALL - Consultar todos los puntajes
+    System.out.println("Consultar todos los puntajes...");
+    String allScores = scoreController.all();
+    System.out.println(allScores);
+    System.out.println("\n");
+    // READ BY INDEX - Consultar puntaje por índice
+    System.out.println("Se ha consultado el puntaje con el indice 1");
+    String scoreByIndex = scoreController.findByIndex(1);
+    System.out.println(scoreByIndex);
+    System.out.println("\n");
+    
+    // UPDATE - Actualizar puntaje del estudiante en índice 0
+    System.out.println("Actualizamos el puntaje del estudiante con indice 0 (sumamos 10 puntos).");
+    String updateScore = scoreController.update(0, 1010, 10);
+    System.out.println(updateScore);
+    System.out.println("\n");
+    // Verificar el cambio
+    System.out.println("Verificamos todos los puntajes despues de la actualizacion:");
+    String allScoresAfterUpdate = scoreController.all();
+    System.out.println(allScoresAfterUpdate);
+    System.out.println("\n");
+    // DELETE - Eliminar puntaje
+    System.out.println("Eliminamos el puntaje con indice 2.");
+    String deleteScore = scoreController.delete(2);
+    System.out.println(deleteScore);
+    System.out.println("\n");
+    // Verificar lista final
+    System.out.println("Lista final de puntajes:");
+    String finalScores = scoreController.all();
+    System.out.println(finalScores);
 
-        // READ ALL - Consultar todos los puntajes
-        System.out.println("Consultar todos los puntajes...");
-        String allScores = scoreController.all();
-        System.out.println(allScores);
-        System.out.println("\n");
-
-        // READ BY INDEX - Consultar puntaje por índice
-        System.out.println("Se ha consultado el puntaje con el indice 1");
-        String scoreByIndex = scoreController.findByIndex(1);
-        System.out.println(scoreByIndex);
-        System.out.println("\n");
-
-        
-        // UPDATE - Actualizar puntaje del estudiante en índice 0
-        System.out.println("Actualizamos el puntaje del estudiante con indice 0 (sumamos 10 puntos).");
-        String updateScore = scoreController.update(0, 1010, 10);
-        System.out.println(updateScore);
-        System.out.println("\n");
-
-        // Verificar el cambio
-        System.out.println("Verificamos todos los puntajes despues de la actualizacion:");
-        String allScoresAfterUpdate = scoreController.all();
-        System.out.println(allScoresAfterUpdate);
-        System.out.println("\n");
-
-        // DELETE - Eliminar puntaje
-        System.out.println("Eliminamos el puntaje con indice 2.");
-        String deleteScore = scoreController.delete(2);
-        System.out.println(deleteScore);
-        System.out.println("\n");
-
-        // Verificar lista final
-        System.out.println("Lista final de puntajes:");
-        String finalScores = scoreController.all();
-        System.out.println(finalScores);
+    //######################################################### MARIO #########################################################
+    System.out.println("Se procede a crear un Comentario:");
+    String feedbackOne = feedbackController.create("Mario munera","La pagina esta muy buena",true);
+    System.out.println(feedbackOne);
+    //######################################################### SERGIO #########################################################
+    //######################################################### JULIO #########################################################
   }
 
 }
