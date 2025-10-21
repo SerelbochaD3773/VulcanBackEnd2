cdd4eeb3dbbce403bd8e91174ec890506856f87d
MarioMunera <samuelmariomunera@gmail.com>
2025-10-21 14:09:10 -0500

    Refactor feedback management system: implement create, read, update, and delete functionalities in FeedbackController and FeedbackUseCase; update App.java for feedback operations; remove unused classes.

--- src/feedback/controllers/FeedbackController.java
+++ src/feedback/controllers/FeedbackController.java
@@ -1,40 +1,42 @@
 package feedback.controllers;
 
-import feedback.useCases.FeedbackUseCase; // Importacion de la clase FeedbackUseCase
-import feedback.models.Feedback; // Importacion de la clase Feedback
+import feedback.useCases.FeedbackUseCase;
 
 public class FeedbackController {
 
-    public FeedbackUseCase feedbackUseCase; // Declaracion de la clase TaskUseCase se convierte como tipo objecto
+    public FeedbackUseCase feedbackUseCase;
 
-    // Constructor
-    public FeedbackController() {
-        this.feedbackUseCase = new FeedbackUseCase(); // Instancia de la clase FeedbackUseCase
+    //contructor
+    public FeedbackController(){
+        //instanciamos clase useCase
+        this.feedbackUseCase = new FeedbackUseCase();
     }
 
     // en ruta la accion de obtener todos
     public String all(){
-        return this.feedbackUseCase.all(); // llamar todas las tareas.
+        return this.feedbackUseCase.all(); // trae todos los comentarios
     }
 
     // en ruta la accion de obtener por indice
     public String findByIndex(int index){
-        return this.feedbackUseCase.findByIndex(index); // llamar la accion de buscar por indice.
+        return this.feedbackUseCase.findByIndex(index); // Trae comentario x indice
     }
 
     // en ruta la accion de crear
-    public String create(String name, String description, boolean status){
-        return this.feedbackUseCase.create(new Feedback()); // llamar la accion de crear tarea.
+    public String create(String email, String phone, String comment ){
+        return this.feedbackUseCase.create(email,phone,comment);
     }
 
     // en ruta la accion de actualizar
-    public String update(int index, String name, String description, boolean status){
-        return this.feedbackUseCase.update(index, new Feedback ()); // llamar la accion de actualizar tarea.
+    public String update(int index, String email, String phone, String comment){
+        return this.feedbackUseCase.update(index, email, phone, comment); //Actualiza email phone y comment si el usuario lo desea.
     }
 
     // en ruta la accion de eliminar
     public String delete (int index){
         return this.feedbackUseCase.delete(index);
     }
+    
+
 
 }
